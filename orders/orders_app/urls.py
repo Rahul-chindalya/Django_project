dce8de53c23fd349bf197687533a93e86fdef17e
Rahul-chindalya <rahulchindalya01@gmail.com>
2026-02-07 15:27:09 +0530

    update and delete using class based view and understood the CBV

--- orders/orders_app/urls.py
+++ orders/orders_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
     path('products/add/',views.ProductCreateView.as_view(),name='add_product'),
-    path('product/list/',views.ProductListView.as_view(),name='products_list')
+    path('product/list/',views.ProductListView.as_view(),name='products_list'),
+    path('product/detail/<int:pk>/',views.ProductDetailView.as_view(),name='product_detail'),
+    path('Product/update/<int:pk>/',views.ProductUpdateView.as_view(),name='update_product'),
+    path('Product/delete/<int:pk>/',views.ProductDeleteView.as_view(),name='product_delete'),
 ]
--- orders/orders_app/views.py
+++ orders/orders_app/views.py
@@ -11,9 +11,26 @@ class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'add_product.html'
-    success_url = reverse_lazy('prodcuts_list')
+    success_url = reverse_lazy('products_list')
 
 class ProductListView(ListView):
     model = Product
     template_name = 'products_list.html'
     context_object_name = 'products'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'detail_product.html'
+    context_object_name = 'product' 
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'update_product.html'
+    success_url = reverse_lazy('products_list')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'delete_product.html'
+    success_url = reverse_lazy('products_list')
